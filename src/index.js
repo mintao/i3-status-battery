910ad2bd48547a58e4d8eecfe7d6ae62e6ebc096
Florian Fackler <florian@fackler.cloud>
2019-07-10 15:08:54 +0200

    Implement critical threshold

--- src/index.js
+++ src/index.js
@@ -4,27 +4,32 @@ import { EventEmitter } from 'events';
 import systeminfo from 'systeminformation';
 
 export default class Text extends EventEmitter {
+
   constructor(options, output) {
     super();
     options = options || {};
     this.output = output || {};
+    this.criticalThreshold = options.criticalThreshold || 18;
   }
 
   update() {
-    let msg = ':)';
-    this.output.critical = true;
+    let msg = '';
 
     systeminfo.battery(info => {
       msg = info.percent + '%';
       if (!info.ischarging) {
         msg += ' (' + this.formatMinutes(info.timeremaining) + ')';
       }
-
-      this.output.critical = false;
-
+      if (info.percent < this.criticalThreshold) {
+        this.output.critical = true;
+        this.output.color = '#FF4444';
+      }
+    }).catch(err => {
+      this.output.critical = true;
+      msg = err;
+    }).finally(() => {
       this.output.full_text = msg;
       this.output.short_text = msg;
-
       this.emit('updated', this, this.output);
     })
   }
@@ -39,7 +44,11 @@ export default class Text extends EventEmitter {
       h = Math.floor(min / 60);
       min = min%60;
     }
-    return `${h}:${min}`;
+    return `${this.format(h)}:${this.format(min)}`;
+  }
+
+  format(num) {
+    return num < 10 ? `0${num}` : num;
   }
 }
 
