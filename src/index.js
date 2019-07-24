97abac531a2a79f945f113be58ef5c5c6c94a073
Florian Fackler <florian@fackler.cloud>
2019-07-24 09:57:56 +0200

    Supress time display if it has a negative value

--- src/index.js
+++ src/index.js
@@ -1,46 +1,49 @@
-'use strict';
+"use strict";
 
-import { EventEmitter } from 'events';
-import systeminfo from 'systeminformation';
+import { EventEmitter } from "events";
+import systeminfo from "systeminformation";
 
 export default class Text extends EventEmitter {
-
   constructor(options, output) {
     super();
     options = options || {};
     this.output = output || {};
-    this.chargingColor = options.chargingColor || '#666666';
-    this.dischargingColor = options.dischargingColor || '#AAAA00';
+    this.chargingColor = options.chargingColor || "#666666";
+    this.dischargingColor = options.dischargingColor || "#AAAA00";
     this.criticalThreshold = options.criticalThreshold || 30;
   }
 
   update() {
-    systeminfo.battery(info => {
-      let msg = info.percent + '%';
-      if (!info.ischarging && info.percent !== 100) {
-        msg += ' (' + this.formatMinutes(info.timeremaining) + ')';
-        if (info.timeremaining < this.criticalThreshold) {
-          this.output.critical = true;
+    systeminfo
+      .battery(info => {
+        let msg = info.percent + "%";
+        if (!info.ischarging && info.percent !== 100) {
+          if (info.timeremaining > 0) {
+            msg += " (" + this.formatMinutes(info.timeremaining) + ")";
+          }
+          if (info.timeremaining < this.criticalThreshold) {
+            this.output.critical = true;
+          }
         }
-      }
 
-      this.output.full_text = msg;
-      this.output.short_text = msg;
+        this.output.full_text = msg;
+        this.output.short_text = msg;
 
-      // Color indicating loading status
-      if (info.ischarging === true) {
-        this.changeColor(this.chargingColor);
-      } else {
-        this.changeColor(this.dischargingColor);
-      }
-
-    }).catch(err => {
-      this.output.critical = true;
-      this.output.full_text = err;
-      this.output.short_text = err;
-    }).finally(msg => {
-      this.emit('updated', this, this.output);
-    })
+        // Color indicating loading status
+        if (info.ischarging === true) {
+          this.changeColor(this.chargingColor);
+        } else {
+          this.changeColor(this.dischargingColor);
+        }
+      })
+      .catch(err => {
+        this.output.critical = true;
+        this.output.full_text = err;
+        this.output.short_text = err;
+      })
+      .finally(msg => {
+        this.emit("updated", this, this.output);
+      });
   }
 
   changeColor(hex) {
@@ -50,16 +53,19 @@ export default class Text extends EventEmitter {
   }
 
   formatMinutes(min) {
+    if (min < 0) {
+      return min;
+    }
     let h = 0;
     if (min > 60) {
       h = Math.floor(min / 60);
-      min = min%60;
+      min = min % 60;
     }
     return `${this.format(h)}:${this.format(min)}`;
   }
 
   format(num) {
+    return num;
     return num < 10 ? `0${num}` : num;
   }
 }
-
